9edf8f502cf8868649eda9528e276ffe5b5f9072
Igordevz <igortza.98483@gmail.com>
2023-03-29 08:30:14 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import  express, { application }  from "express";
 import { ConnectDb } from "./database/database_config";
 import { router } from "./router/router";
-import cors from "cors";
 async function Botstrap(){
 
 
     const app = express();
     app.use(express.json());
-    app.use(cors())
     await ConnectDb();
 
     const Door = 3000
